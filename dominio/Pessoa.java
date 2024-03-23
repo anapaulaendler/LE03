35c1e9ef3a3c90c3629425b1e474bd81dbcc3743
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 19:29:16 -0300

    Exercício Treze

--- dominio/Pessoa.java
+++ dominio/Pessoa.java
@@ -3,4 +3,18 @@ package dominio;
 public class Pessoa {
     public String[] nome;
     public char[] sexo;
+    public int[] idade;
+    public boolean[] saude;
+
+    public Pessoa() {}
+    public Pessoa(int tamanho) {
+        nome = new String[tamanho];
+        sexo = new char[tamanho];
+        idade = new int[tamanho];
+        saude = new boolean[tamanho];
+    }
+
+    public boolean apto(int indice) {
+        return (idade[indice] >= 18 && idade[indice] <= 30 && saude[indice]);
+    }
 }
