1fa65fe375ebaff7031c010b3d17e1ce330365d7
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 19:48:55 -0300

    Correções

--- ExercicioQuatorze.java
+++ ExercicioQuatorze.java
@@ -6,19 +6,19 @@ public class ExercicioQuatorze {
         como resultado se houve lucro, prejuízo ou empate para cada produto. Informe média de preço de
         custo e do preço de venda. */
 
-        Produto produto = new Produto(3);
+        Produto produto = new Produto(40);
         double mpc = 0;
         double mpv = 0;
         double lucro = 0;
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 40; i++) {
             produto.pc[i] = Prompt.lerDecimal("Digite o preço de custo: ");
             mpc += produto.pc[i];
             produto.pv[i] = Prompt.lerDecimal("Digite o preço de venda: ");
             mpv += produto.pv[i];
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 40; i++) {
             lucro = produto.l(i);
             if (lucro > 0) {
                 System.out.println("O produto número " + (i + 1) + " está dando lucro.");
@@ -29,8 +29,8 @@ public class ExercicioQuatorze {
             }
         }
 
-        System.out.printf("A média de preço de custo é de R$ %.2f.\nA média de preço de venda é de: R$ %.2f.\n", (mpc/3), (mpv/3));
+        System.out.printf("A média de preço de custo é de R$ %.2f.\nA média de preço de venda é de: R$ %.2f.\n", (mpc/40), (mpv/40));
         Prompt.separador();
-        
+
     }
 }
