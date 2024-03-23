b8c16178c02fc038558ee469ecb89860bc2abc1d
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 16:21:40 -0300

    Exercício Nove

---
+++ ExercicioNove.java
@@ -0,0 +1,16 @@
+public class ExercicioNove {
+    public static void executar() {
+        int contador = 0;
+        
+        for (int i = 0; i < 80; i++) {
+            int numero = Prompt.lerInteiro("Digite um número: ");
+            
+            if (numero >= 10 && numero <= 150) {
+                contador++;
+            }
+        }
+        
+        System.out.println("Quantidade de números no intervalo [10, 150]: " + contador);
+
+    }
+}
