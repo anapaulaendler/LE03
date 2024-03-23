75f0c48da13b050c16a2aa46f18f7324cc8a4929
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 15:15:50 -0300

    Exercicio Cinco

---
+++ ExercicioCinco.java
@@ -0,0 +1,18 @@
+import dominio.Loja;
+
+public class ExercicioCinco {
+    public static void executar() {
+        /* A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
+        Faça um programa que receba um valor de uma compra e mostre o valor das prestações. */
+
+        System.out.print("Bem-vindo a loja Mamão com Açúcar! :)\n");
+        double a = Prompt.lerDecimal("Digite o valor de sua compra! ");
+
+        Loja mamao = new Loja();
+        double p = mamao.ps(a, 5);
+
+        System.out.printf("O valor de cada prestação é de: R$ %.2f\n", p);
+        Prompt.separador();
+        
+    }
+}
---
+++ dominio/Loja.java
@@ -0,0 +1,9 @@
+package dominio;
+
+public class Loja {
+
+    public double ps(double preco, double p) {
+        return preco / p;
+    }
+    
+}
