0b84743086d54498eb622cfcca03a02c4598f5f3
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 13:44:22 -0300

    Primeiro Exercício

---
+++ ExercicioUm.java
@@ -0,0 +1,21 @@
+import dominio.*;
+
+public class ExercicioUm {
+    public static void executar() {
+        /* Escrever um programa que leia o nome de um aluno e as notas das três provas que ele obteve
+        no semestre. No final informar o nome do aluno e a sua média (aritmética). */
+
+        Aluno alunoUm = new Aluno();
+        alunoUm.nome = Prompt.lerLinha("Digite o nome do aluno: ");
+
+        alunoUm.notas = new double[3];
+        double media = 0;
+
+        for (int i = 0; i < 3; i++) {
+            alunoUm.notas[i] = Prompt.lerDecimal("Digite sua nota: ");
+            media += alunoUm.notas[i];
+        }
+
+        System.out.printf("A média do aluno %s é: %.2f", alunoUm.nome, (media/3));
+    }
+}
---
+++ dominio/Aluno.java
@@ -0,0 +1,24 @@
+package dominio;
+
+public class Aluno {
+
+    public String matricula;
+    public String nome;
+    public double[] notas;
+
+    //Método construtor
+    public Aluno(){
+    }
+
+    //Método construtor com parâmetro
+    public Aluno(String nome){
+        this.nome = nome;        
+    }
+
+    //Método construtor com parâmetros
+    public Aluno(String nome, String matricula){
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+}
