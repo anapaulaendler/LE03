c6492caee462a4f1e4a9fdbbffe4656c6dbec317
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 21:36:04 -0300

    Exercicio Dezenove

---
+++ ExercicioDezenove.java
@@ -0,0 +1,31 @@
+import dominio.Triangulo;
+
+public class ExercicioDezenove {
+    public static void executar() {
+        /* . Escrever um programa que leia três valores inteiros e verifique se eles podem ser os lados de
+        um triângulo. Se forem, informar qual o tipo de triângulo que eles formam: equilátero, isóscele ou
+        escaleno.
+        Propriedade: o comprimento de cada lado de um triângulo é menor do que a soma dos
+        comprimentos dos outros dois lados.
+        Triângulo Equilátero: aquele que tem os comprimentos dos três lados iguais;
+        Triângulo Isóscele: aquele que tem os comprimentos de dois lados iguais. Portanto, todo
+        triângulo equilátero é também isóscele;
+        Triângulo Escaleno: aquele que tem os comprimentos de seus três lados diferentes. */
+
+        Triangulo triangulo = new Triangulo();
+        triangulo.a = Prompt.lerInteiro("Digite o primeiro valor: ");
+        triangulo.b = Prompt.lerInteiro("Digite o segundo valor: ");
+        triangulo.c = Prompt.lerInteiro("Digite o terceiro valor: ");
+
+        int i = triangulo.verificar(triangulo.a, triangulo.b, triangulo.c);
+        if (i == 1) {
+            String tipo = triangulo.lados(triangulo.a, triangulo.b, triangulo.c);
+            System.out.println("O triângulo é do tipo: " + tipo);
+        } else {
+            System.out.println("Não é triângulo.");
+        }
+
+        Prompt.separador();
+
+    }
+}
