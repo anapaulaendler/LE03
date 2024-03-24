d44b8c4fa0237f22e36f7623602db861e6304a43
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 21:58:40 -0300

    Vinte e Um

---
+++ ExercicioVinteEUm.java
@@ -0,0 +1,20 @@
+import dominio.Nadador;
+
+public class ExercicioVinteEUm {
+    public static void executar() {
+        /* Elabore um programa que, dada a idade de um nadador. Classifique-o em uma das seguintes categorias:
+        Infantil A = 5 - 7 anos
+        Infantil B = 8 - 10 anos
+        juvenil A = 11- 13 anos
+        juvenil B = 14 - 17 anos
+        Sênior = 18 - 25 anos
+        Apresentar mensagem “idade fora da faixa etária” quando for outro ano não contemplado. */
+
+        Nadador nadador = new Nadador();
+        nadador.idade = Prompt.lerInteiro("Digite a idade do nadador: ");
+
+        Nadador.classificarNadador(nadador.idade);
+        Prompt.separador();
+
+    }
+}
--- Programa.java
+++ Programa.java
@@ -1,5 +1,5 @@
 public class Programa {
     public static void main(String[] args) {
-        ExercicioVinte.executar();
+        ExercicioVinteEUm.executar();
     }
 }
