a2e302520e0326cbc8531ceef87b91414768809b
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 13:10:51 -0300

    Renomear 04

--- Exercicio04.java
+++ Exercicio04.java
@@ -1,6 +1,6 @@
 import dominio.*;
 
-public class ExercicioQuatro {
+public class Exercicio04 {
     public static void executar() {
         /* Elaborar um programa que efetue a apresentação do valor da conversão em real (R$) de um
         valor lido em dólar (US$). O programa deverá solicitar o valor da cotação do dólar e também a
