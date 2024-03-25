6351c5f53ceeb4a95cc56b3697e56a1303eceac6
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 13:20:01 -0300

    Renomear 23

--- Exercicio23.java
+++ Exercicio23.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import dominio.Pessoa;
 
-public class ExercicioVinteETres {
+public class Exercicio23 {
     public static void executar() {
         /* Faça um programa que leia o nome, o sexo, a altura e a idade de uma pessoa. Calcule e mostre
         nome e o seu peso ideal de acordo com as seguintes características da pessoa: */
