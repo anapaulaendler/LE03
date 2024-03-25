f263bea82cefa7add8053ca0b82fdc47c1a544ac
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 13:15:14 -0300

    Renomear 03

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,6 +1,6 @@
 import dominio.Funcionario;
 
-public class ExercicioTres {
+public class Exercicio03 {
     public static void executar() {
         /* Escrever um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas
         efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre
