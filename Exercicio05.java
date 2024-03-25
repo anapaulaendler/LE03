5763c626b019b030ab1d9b6666de85eed2dacae4
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 12:48:30 -0300

    Renomear 05

--- Exercicio05.java
+++ Exercicio05.java
@@ -1,6 +1,6 @@
 import dominio.Loja;
 
-public class ExercicioCinco {
+public class Exercicio05 {
     public static void executar() {
         /* A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
         Faça um programa que receba um valor de uma compra e mostre o valor das prestações. */
