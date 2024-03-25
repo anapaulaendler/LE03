b62010a0b015028642ce8ee4ceed3b0632427390
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 13:14:56 -0300

    Renomear 07

--- Exercicio07.java
+++ Exercicio07.java
@@ -1,6 +1,6 @@
 import dominio.*;
 
-public class ExercicioSete {
+public class Exercicio07 {
     public static void executar() {
         /* O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do
         distribuidor e dos impostos (aplicados, primeiro os impostos sobre o custo de fábrica, e depois a
