8a63cb74e3176344c28065a3cf0d527c076ccfc2
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 13:06:22 -0300

    Renomear 17

--- Exercicio17.java
+++ Exercicio17.java
@@ -1,6 +1,6 @@
 import dominio.Funcionario;
 
-public class ExercicioDezesste {
+public class Exercicio17 {
     public static void executar() {
         /*  Leia o nome do funcionário, seu salário e o valor do salário mínimo. Calcule o seu novo salário
         reajustado. Escrever o nome do funcionário, o reajuste e seu novo salário. Calcule quanto à
