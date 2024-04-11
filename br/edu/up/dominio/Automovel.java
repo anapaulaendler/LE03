9b0eeeff7345b635421b92569f5c477e116cf4f7
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-11 00:46:43 -0300

    Privando: Automovel

--- br/edu/up/dominio/Automovel.java
+++ br/edu/up/dominio/Automovel.java
@@ -4,24 +4,61 @@ public class Automovel {
 
     private double litros;
     private double distancia;
+    private String tipoCombustivel;
+    private double valor;
+    
+    // Métodos construtores:
+
+    public Automovel() {}
     
     public Automovel(double litros, double distancia){
         this.litros = litros;
         this.distancia = distancia;
     }
 
-    public double cm() {
-        return distancia / litros;
-    }
-
-    public String tipoCombustivel;
-    public double valor;
-
-    public Automovel() {}
-
     public Automovel(String tipoCombustivel, double valor) {
         this.tipoCombustivel = tipoCombustivel;
         this.valor = valor;
+    }
+
+    // Getters and Setters
+
+    public double getLitros() {
+        return litros;
+    }
+
+    public void setLitros(double litros) {
+        this.litros = litros;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+    
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    // Funcões
+
+    public double cm() {
+        return distancia / litros;
     }
 
     public double calcularDesconto() {
