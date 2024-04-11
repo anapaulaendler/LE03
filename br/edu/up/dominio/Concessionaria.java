da1feaff425dcac7e966b9a949f025d8d3e2f238
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-11 00:55:35 -0300

    Privando: Concessionária

--- br/edu/up/dominio/Concessionaria.java
+++ br/edu/up/dominio/Concessionaria.java
@@ -2,17 +2,72 @@ package br.edu.up.dominio;
 
 public class Concessionaria {
     
+    private int ano;
+    private double preco;
+    private int cg;
+    private int c2000;
+
+    // Métodos Construtores:
+
+    public Concessionaria() {
+    }
+
+    public Concessionaria(int ano, double preco) {
+        this.ano = ano;
+        this.preco = preco;
+    }
+
+    public Concessionaria(int ano) {
+        this.ano = ano;
+    }
+
+    public Concessionaria(double preco) {
+        this.preco = preco;
+    }
+
+    // Getters and Setters
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getCg() {
+        return cg;
+    }
+
+    public void setCg(int cg) {
+        this.cg = cg;
+    }
+
+    public int getC2000() {
+        return c2000;
+    }
+
+    public void setC2000(int c2000) {
+        this.c2000 = c2000;
+    }
+
+    // Funções
+
+
     public double custoC (double cf) {
         double i = cf * 0.45;
         double pd = 0.28 * (cf + i);
         double cc = pd + i + cf;
         return cc;
     }
-
-    public int ano;
-    public double preco;
-    public int cg;
-    public int c2000;
 
     public void desconto(int ano, double preco) {
         this.ano = ano;
