0f1db5a1250942ff2ddfcf4e77c7008c2e4e4975
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-11 01:07:16 -0300

    Privando: Consumo

--- br/edu/up/dominio/Consumo.java
+++ br/edu/up/dominio/Consumo.java
@@ -2,9 +2,56 @@ package br.edu.up.dominio;
 
 public class Consumo {
     
-    public int id;
-    public int consumo;
-    public double conta;
+    private int id;
+    private int consumo;
+    private double conta;
+
+    // Métodos Construtores:
+
+    public Consumo() {
+    }
+
+    public Consumo(int id, int consumo, double conta) {
+        this.id = id;
+        this.consumo = consumo;
+        this.conta = conta;
+    }
+
+    public Consumo(double conta) {
+        this.conta = conta;
+    }
+
+    public Consumo(int id) {
+        this.id = id;
+    }
+
+    // Getters and Setters:
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(int consumo) {
+        this.consumo = consumo;
+    }
+
+    public double getConta() {
+        return conta;
+    }
+
+    public void setConta(double conta) {
+        this.conta = conta;
+    }
+
+    // Funções:
 
     public void valor() {
         if (id == 1) {
