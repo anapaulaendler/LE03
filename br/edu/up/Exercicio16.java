1826dca723057bf3dfba89ccb790087b4b7c65ad
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-25 10:28:35 -0300

    Ex16

--- br/edu/up/Exercicio16.java
+++ br/edu/up/Exercicio16.java
@@ -3,27 +3,39 @@ import br.edu.up.dominio.*;
 
 public class Exercicio16 {
 
-    
     public static void executar() {
+
         /* Escrever um programa para uma empresa que decide dar um reajuste a seus 584 funcionários
         de acordo com os seguintes critérios:
         a) 50% para aqueles que ganham menos do que três salários mínimos;
         b) 20% para aqueles que ganham entre três até dez salários mínimos;
         c) 15% para aqueles que ganham acima de dez até vinte salários mínimos;
         d) 10% para os demais funcionários. */
+
         int n = 584;
         Funcionario funcionario = new Funcionario(n);
 
-        funcionario.salarioMinimo = Prompt.lerDecimal("Informe o valor do salário mínimo: ");
+        double salarioMinimo = Prompt.lerDecimal("Informe o valor do salário mínimo: ");
+        funcionario.setSalarioMinimo(salarioMinimo);
+
+        String[] nome = new String[n];
+        double[] s = new double[n];
+        double[] ns = new double[n];
 
         for (int i = 0; i < n; i++) {
-            funcionario.n[i] = Prompt.lerLinha("Digite o nome do funcionário: ");
-            funcionario.s[i] = Prompt.lerDecimal("Digite o salário atual do funcionário: ");
-            funcionario.ns[i] = funcionario.novoS(i);
+            nome[i] = Prompt.lerLinha("Digite o nome do funcionário: ");
+            funcionario.setN(nome);
+
+            s[i] = Prompt.lerDecimal("Digite o salário atual do funcionário: ");
+            funcionario.setS(s);
+
+            ns[i] = funcionario.novoS(i);
+            funcionario.setNs(ns);
+
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("Nome: %s\nNovo salário: R$ %.2f\n", funcionario.n[i], funcionario.ns[i]);
+            System.out.printf("Nome: %s\nNovo salário: R$ %.2f\n", funcionario.getN()[i], funcionario.getNs()[i]);
         }
 
         Prompt.separador();
--- br/edu/up/dominio/Funcionario.java
+++ br/edu/up/dominio/Funcionario.java
@@ -7,28 +7,25 @@ public class Funcionario {
     private double totv;
     private char sexo;
     private int idade;
+
     private String[] n;
     private double[] s;
     private double[] ns;
+
     private double salarioMinimo;
+    
     private int reajuste;
     private int nivel;
     private int horas;
     private double salario;
 
+    // métodos construtores:
 
     public Funcionario(String nome, double salf, double totv) {
         this.nome = nome;
         this.salf = salf;
         this.totv = totv;
     }
-
-    public double salario() {
-        double conta = salf + (totv * 0.15);
-        return conta;
-    }
-
-    //
 
     public Funcionario(int tamanho) {
         n = new String[tamanho];
@@ -37,6 +34,13 @@ public class Funcionario {
     }
 
     public Funcionario() {}
+
+    // funções:
+
+    public double salario() {
+        double conta = salf + (totv * 0.15);
+        return conta;
+    }
 
     public double novoS(int indice) {
 
@@ -50,15 +54,11 @@ public class Funcionario {
             return s[indice] * 1.1; 
         }
     }
-    
-    //
 
     public double novoS2(int indice) {
         ns[indice] = s[indice] * (1 + (reajuste / 100.0));
         return ns[indice];
     }
-
-    //
     
         public double pagamento() {
             if (nivel == 1) {
@@ -72,4 +72,113 @@ public class Funcionario {
             }
             return salario;
         }
+
+        // getters e setters:
+        
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public double getSalf() {
+            return salf;
+        }
+
+        public void setSalf(double salf) {
+            this.salf = salf;
+        }
+
+        public double getTotv() {
+            return totv;
+        }
+
+        public void setTotv(double totv) {
+            this.totv = totv;
+        }
+
+        public char getSexo() {
+            return sexo;
+        }
+
+        public void setSexo(char sexo) {
+            this.sexo = sexo;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public String[] getN() {
+            return n;
+        }
+
+        public void setN(String[] n) {
+            this.n = n;
+        }
+
+        public double[] getS() {
+            return s;
+        }
+
+        public void setS(double[] s) {
+            this.s = s;
+        }
+
+        public double[] getNs() {
+            return ns;
+        }
+
+        public void setNs(double[] ns) {
+            this.ns = ns;
+        }
+
+        public double getSalarioMinimo() {
+            return salarioMinimo;
+        }
+
+        public void setSalarioMinimo(double salarioMinimo) {
+            this.salarioMinimo = salarioMinimo;
+        }
+
+        public int getReajuste() {
+            return reajuste;
+        }
+
+        public void setReajuste(int reajuste) {
+            this.reajuste = reajuste;
+        }
+
+        public int getNivel() {
+            return nivel;
+        }
+
+        public void setNivel(int nivel) {
+            this.nivel = nivel;
+        }
+
+        public int getHoras() {
+            return horas;
+        }
+
+        public void setHoras(int horas) {
+            this.horas = horas;
+        }
+
+        public double getSalario() {
+            return salario;
+        }
+
+        public void setSalario(double salario) {
+            this.salario = salario;
+        }
     }
+
+
+    
