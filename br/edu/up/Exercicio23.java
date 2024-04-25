f2259f7dbbf6d5b78d0a21cc444820cd8192b229
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-25 10:54:12 -0300

    ex23

--- br/edu/up/Exercicio23.java
+++ br/edu/up/Exercicio23.java
@@ -5,7 +5,7 @@ import br.edu.up.dominio.Pessoa;
 
 public class Exercicio23 {
     public static void executar() {
-        
+
         /* Faça um programa que leia o nome, o sexo, a altura e a idade de uma pessoa. Calcule e mostre
         nome e o seu peso ideal de acordo com as seguintes características da pessoa: */
 
@@ -14,12 +14,21 @@ public class Exercicio23 {
         Pessoa pessoa = new Pessoa();
 
         System.out.println("Informe o sexo da pessoa (F ou M): ");
-        pessoa.s = leitor.nextLine().toUpperCase().charAt(0);
-        pessoa.a = Prompt.lerDecimal("Informe a altura da pessoa, em m: ");
-        pessoa.i = Prompt.lerInteiro("Informe a idade da pessoa: ");
-        pessoa.p = pessoa.pesoIdeal();
 
-        System.out.printf("Peso Ideal: %.2f\n", pessoa.p);
+        char s = leitor.nextLine().toUpperCase().charAt(0);
+        pessoa.setS(s);
+
+        double a = Prompt.lerDecimal("Informe a altura da pessoa, em m: ");
+        pessoa.setA(a);
+
+        int i = Prompt.lerInteiro("Informe a idade da pessoa: ");
+        pessoa.setI(i);
+
+        double p = pessoa.pesoIdeal();
+        pessoa.setP(p);
+
+        System.out.printf("Peso Ideal: %.2f\n", pessoa.getP());
+        
         Prompt.separador();
         
     }
