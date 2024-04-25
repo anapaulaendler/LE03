d7ab04f50c4d39e960c5383757d0f30d3b80e7f1
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-25 10:39:54 -0300

    ex18

--- br/edu/up/Exercicio18.java
+++ br/edu/up/Exercicio18.java
@@ -5,34 +5,54 @@ import br.edu.up.dominio.Funcionario;
 
 public class Exercicio18 {
     public static void executar() {
+
         /* . Faça um programa que receba o nome a idade, o sexo e salário fixo de um funcionário. Mostre
         o nome e o salário líquido acrescido do abono conforme o sexo e a idade: */
 
-        
         Scanner leitor = new Scanner(System.in);
         Funcionario funcionario = new Funcionario();
 
-        funcionario.nome = Prompt.lerLinha("Digite o nome do funcionário: ");
-        funcionario.salf = Prompt.lerDecimal("Digite o salário fixo do funcionário: ");
-        System.out.println("Digite o sexo da pessoa (F ou M): ");
-        funcionario.sexo = leitor.nextLine().toUpperCase().charAt(0);
-        funcionario.idade = Prompt.lerInteiro("Digite a idade do funcionário: ");
+        String nome = Prompt.lerLinha("Digite o nome do funcionário: ");
+        funcionario.setNome(nome);
 
-        if (funcionario.sexo == 'F') {
-            if (funcionario.idade < 30) {
-                funcionario.salf += 80.0;
+        double salf = Prompt.lerDecimal("Digite o salário fixo do funcionário: ");
+        funcionario.setSalf(salf);
+
+        System.out.println("Digite o sexo da pessoa (F ou M): ");
+
+        char sexo = leitor.nextLine().toUpperCase().charAt(0);
+        funcionario.setSexo(sexo);
+
+        int idade = Prompt.lerInteiro("Digite a idade do funcionário: ");
+        funcionario.setIdade(idade);
+
+        if (funcionario.getSexo() == 'F') {
+            if (funcionario.getIdade() < 30) {
+
+                salf = funcionario.getSalf() + 80.0;
+                funcionario.setSalf(salf);
+
             } else {
-                funcionario.salf += 200.0;
+
+                salf = funcionario.getSalf() + 200.0;
+                funcionario.setSalf(salf);
+
             }
-        } else if (funcionario.sexo == 'M') {
-            if (funcionario.idade < 30) {
-                funcionario.salf += 50.0;
+        } else if (funcionario.getSexo() == 'M') {
+            if (funcionario.getIdade() < 30) {
+
+                salf = funcionario.getSalf() + 50.0;
+                funcionario.setSalf(salf);
+
             } else {
-                funcionario.salf += 100.0;
+
+                salf = funcionario.getSalf() + 100.0;
+                funcionario.setSalf(salf);
+
             } 
         }
 
-        System.out.printf("O novo salário de %s, conforme seu sexo e idade, é de: R$ %.2f\n", funcionario.nome, funcionario.salf);
+        System.out.printf("O novo salário de %s, conforme seu sexo e idade, é de: R$ %.2f\n", funcionario.getNome(), funcionario.getSalf());
         Prompt.separador();
 
     }
