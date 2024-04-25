02112a3a5092da01398a42a27770c92c031308c0
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-25 10:17:25 -0300

    EX14

--- br/edu/up/Exercicio14.java
+++ br/edu/up/Exercicio14.java
@@ -3,24 +3,34 @@ import br.edu.up.dominio.*;
 
 public class Exercicio14 {
     public static void executar() {
-        
+
         /* Faça um programa que receba o preço de custo e o preço de venda de 40 produtos. Mostre
         como resultado se houve lucro, prejuízo ou empate para cada produto. Informe média de preço de
         custo e do preço de venda. */
 
-        Produto produto = new Produto(40);
+        int n = 40;
+
+        Produto produto = new Produto(n);
         double mpc = 0;
         double mpv = 0;
         double lucro = 0;
+
+        double[] pC = new double [n];
+        double[] pV = new double[n];
         
-        for (int i = 0; i < 40; i++) {
-            produto.pc[i] = Prompt.lerDecimal("Digite o preço de custo: ");
-            mpc += produto.pc[i];
-            produto.pv[i] = Prompt.lerDecimal("Digite o preço de venda: ");
-            mpv += produto.pv[i];
+        for (int i = 0; i < n; i++) {
+            pC[i] = Prompt.lerDecimal("Digite o preço de custo: ");
+            produto.setPc(pC);
+
+            mpc += produto.getPc()[i];
+
+            pV[i] = Prompt.lerDecimal("Digite o preço de venda: ");
+            produto.setPv(pV);
+
+            mpv += produto.getPv()[i];
         }
 
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < n; i++) {
             lucro = produto.l(i);
             if (lucro > 0) {
                 System.out.println("O produto número " + (i + 1) + " está dando lucro.");
@@ -31,7 +41,7 @@ public class Exercicio14 {
             }
         }
 
-        System.out.printf("A média de preço de custo é de R$ %.2f.\nA média de preço de venda é de: R$ %.2f.\n", (mpc/40), (mpv/40));
+        System.out.printf("A média de preço de custo é de R$ %.2f.\nA média de preço de venda é de: R$ %.2f.\n", (mpc/n), (mpv/40));
         Prompt.separador();
 
     }
