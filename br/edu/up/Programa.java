184c0ec75409a29a0a1e327fb5fad6b942013650
Ana Paula Endler <anapaulaendler@gmail.com>
2024-04-30 08:29:38 -0300

    testando

--- br/edu/up/Programa.java
+++ br/edu/up/Programa.java
@@ -1,31 +1,31 @@
 package br.edu.up;
 public class Programa {
     public static void main(String[] args) {
-        Exercicio01.executar();
-        Exercicio02.executar();
-        // Exercicio03.executar();
-        // Exercicio04.executar();
-        // Exercicio05.executar();
-        // Exercicio06.executar();
-        // Exercicio07.executar();
-        //Exercicio08.executar();
-        //Exercicio09.executar();
-        //Exercicio10.executar();
-        //Exercicio11.executar();
-        //Exercicio12.executar();
-        //Exercicio13.executar();
-        //Exercicio14.executar();
-        //Exercicio15.executar();
-        //Exercicio16.executar();
-        //Exercicio17.executar();
-        //Exercicio18.executar();
-        //Exercicio19.executar();
-        //Exercicio20.executar();
-        //Exercicio21.executar();
-        //Exercicio22.executar();
-        //Exercicio23.executar();
-        //Exercicio24.executar();
-        //Exercicio25.executar();
-        //Exercicio26.executar();
+         Exercicio01.executar();
+         Exercicio02.executar();
+         Exercicio03.executar();
+         Exercicio04.executar();
+         Exercicio05.executar();
+         Exercicio06.executar();
+         Exercicio07.executar();
+         Exercicio08.executar();
+         Exercicio09.executar();
+         Exercicio10.executar();
+         Exercicio11.executar();
+         Exercicio12.executar();
+         Exercicio13.executar();
+         Exercicio14.executar();
+         Exercicio15.executar();
+         Exercicio16.executar();
+         Exercicio17.executar();
+         Exercicio18.executar();
+         Exercicio19.executar();
+         Exercicio20.executar();
+         Exercicio21.executar();
+         Exercicio22.executar();
+         Exercicio23.executar();
+         Exercicio24.executar();
+         Exercicio25.executar();
+         Exercicio26.executar();
     }
 }
